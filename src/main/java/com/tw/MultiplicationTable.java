6fef916dd22979262ad6d5cdd2ab35a357d0a843
moksi3 <simon.mok@oocl.com>
2021-11-30 16:13:25 +0800

    [mul_table][simon] feat: add all missing block

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,31 +1,62 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int start, int end) {
+    public String create(int start, int end)
+    {
+        if(this.isValid(start , end))
+            return this.generateTable(start , end);
         return null;
     }
 
-    public Boolean isValid(int start, int end) {
-        return null;
+    public Boolean isValid(int start, int end)
+    {
+        return isInRange(start) && isInRange(end) && isStartNotBiggerThanEnd(start , end);
     }
 
-    public Boolean isInRange(int number) {
-        return null;
+    public Boolean isInRange(int number)
+    {
+        return number >= 1 && number <=1000;
     }
 
-    public Boolean isStartNotBiggerThanEnd(int start, int end) {
-        return null;
+    public Boolean isStartNotBiggerThanEnd(int start, int end)
+    {
+        return end >= start;
     }
 
-    public String generateTable(int start, int end) {
-        return null;
+    public String generateTable(int start, int end)
+    {
+        String result = new String();
+
+        for(int row = start ; row <= end ; row++)
+        {
+            result += this.generateLine(start, row);
+            if(row != end)
+                result += "\r\n";
+        }
+
+        return result;
+
     }
 
-    public String generateLine(int start, int row) {
-        return null;
+    public String generateLine(int start, int row)
+    {
+        String result = new String();
+        boolean first = true;
+        for(int i = start ; i <= row ; i++)
+        {
+            if(!first)
+                result += "  ";
+
+            result += this.generateSingleExpression(i , row);
+            first = false;
+
+        }
+        return result;
     }
 
-    public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
+    public String generateSingleExpression(int multiplicand, int multiplier)
+    {
+        int result = multiplicand * multiplier;
+        return String.format("%d*%d=%d" , multiplicand , multiplier , result);
     }
 }
